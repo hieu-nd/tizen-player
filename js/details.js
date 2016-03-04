87d7db35328c7bfc3ded7d441565297ab0e7eea7
hieu duc nguyen <hieu.nguyenduc.hcm@gmail.com>
2016-03-04 15:42:20 +0700

    start tizen player

---
+++ js/details.js
@@ -0,0 +1,52 @@
+function findNextTabStop(el) {
+    var universe = document.querySelectorAll('input, button, select, textarea, a[href], li');
+    var list = Array.prototype.filter.call(universe, function(item) {
+        return item.tabIndex >= "0"
+    });
+    var index = list.indexOf(el);
+    return list[index + 1] || list[0];
+}
+
+function findPreviousTabStop(el) {
+    var universe = document.querySelectorAll('input, button, select, textarea, a[href], li');
+    var list = Array.prototype.filter.call(universe, function(item) {
+        return item.tabIndex >= "0"
+    });
+    var index = list.indexOf(el);
+    return list[index - 1] || list[list.length - 1];
+}
+$(document).ready(function() {
+    $(document).on('keydown', function(e) {
+        var activeNode = document.activeElement;
+        switch (e.which) {
+            case 39:
+            case 40:
+                findNextTabStop(activeNode).focus();
+                break;
+            case 37:
+
+            case 38:
+                findPreviousTabStop(activeNode).focus();
+                break;
+            case 10009:
+                window.location.href = "index.html";
+                break;
+            case 415:
+                //playmedia
+                
+                break;
+            case 19:
+                //pause
+                
+                break;
+            case 417:
+                //fw
+                
+                break;
+            case 412:
+                //rw
+                
+                break;
+        }
+    });
+});
